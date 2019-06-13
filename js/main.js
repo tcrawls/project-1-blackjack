4bc88161735796a75b37bb026231749e5a5753cf
Thompson Rawls <tcrawls@gmail.com>
2019-06-13 11:57:29 -0400

    added div background colors and centered header

--- js/main.js
+++ js/main.js
@@ -2,3 +2,19 @@ function confirm () {
     console.log ('hello world')
 }
 confirm()
+
+
+
+/* <img src="images/back.png" id="card-one">
+<img src="images/back.png" id="card-two">
+<img src="images/back.png" id="card-three">
+<img src="images/back.png" id="card-four">
+<img src="images/back.png" id="card-five">
+<img src="images/back.png" id="card-six">
+
+<img src="images/back.png" id="card-one">
+<img src="images/back.png" id="card-two">
+<img src="images/back.png" id="card-three">
+<img src="images/back.png" id="card-four">
+<img src="images/back.png" id="card-five">
+<img src="images/back.png" id="card-six"> */
