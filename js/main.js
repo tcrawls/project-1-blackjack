8ede29550277b5f836b8214e978a94919f945944
Thompson Rawls <tcrawls@gmail.com>
2019-06-13 15:39:22 -0400

    implemented initial js card arrays  and shuffle function

--- js/main.js
+++ js/main.js
@@ -1,20 +1,87 @@
-function confirm () {
-    console.log ('hello world')
+var cards = [
+    {
+        points: 2,
+        rank: "two",
+        suit: "clubs",
+        cardImage: "images/card-images/2C.png"
+    },
+    {
+        points: 3,
+        rank: "three",
+        suit: "clubs",
+        cardImage: "images/card-images/3C.png"
+    },
+    {
+        points: 4,
+        rank: "four",
+        suit: "clubs",
+        cardImage: "images/card-images/4C.png"
+    },
+    {
+        points: 5,
+        rank: "five",
+        suit: "clubs",
+        cardImage: "images/card-images/5C.png"
+    },
+    {
+        points: 6,
+        rank: "six",
+        suit: "clubs",
+        cardImage: "images/card-images/6C.png"
+    },
+    {
+        points: 7,
+        rank: "seven",
+        suit: "clubs",
+        cardImage: "images/card-images/7C.png"
+    },
+    {
+        points: 8,
+        rank: "eight",
+        suit: "clubs",
+        cardImage: "images/card-images/8C.png"
+    },
+    {
+        points: 9,
+        rank: "nine",
+        suit: "clubs",
+        cardImage: "images/card-images/9C.png"
+    },
+    {
+        points: 10,
+        rank: "ten",
+        suit: "clubs",
+        cardImage: "images/card-images/10C.png"
+    }
+    ];
+    
+var cardsInPlay = []
+var dealerHand = []
+var playerHand = []
+
+// FUNCTION: shuffle deck and store in cardsInPlay
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex -= 1
+      
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex]
+        array[currentIndex] = array[randomIndex]
+        array[randomIndex] = temporaryValue
+    }
+    cardsInPlay = array
+    return cardsInPlay
 }
-confirm()
+      
+function initiateGame() {
+    shuffle(cards)
+
+}
 
 
-
-/* <img src="images/back.png" id="card-one">
-<img src="images/back.png" id="card-two">
-<img src="images/back.png" id="card-three">
-<img src="images/back.png" id="card-four">
-<img src="images/back.png" id="card-five">
-<img src="images/back.png" id="card-six">
-
-<img src="images/back.png" id="card-one">
-<img src="images/back.png" id="card-two">
-<img src="images/back.png" id="card-three">
-<img src="images/back.png" id="card-four">
-<img src="images/back.png" id="card-five">
-<img src="images/back.png" id="card-six"> */
